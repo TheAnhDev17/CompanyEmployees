2b5f76d4cf8a0ab53277ef4b30bd2ff2c8fdeae7
TheAnhDev17 <theanh.dev17@gmail.com>
2024-12-04 23:49:26 +0700

    GetAllCompanies

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -9,5 +9,11 @@ namespace Repository
             :base(repository)
         {
         }
+
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
+            FindAll(trackChanges)
+                .OrderBy(c => c.Name)
+                .ToList();
+
     }
 }
